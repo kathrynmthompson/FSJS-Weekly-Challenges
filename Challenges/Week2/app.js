73df6131271259ee9cf58ef53ddbec165fcbe02c
kathrynmthompson <kthompson680@gmail.com>
2016-09-27 17:54:15 -0400

    attempt at Week 2 Challenge

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -6,3 +6,22 @@
 //Call function
 
 //Alert user results
+
+
+function processInput(message)
+{
+  if message === "Hello")
+}
+{
+  return ("Hello world!");
+}else {
+  return ("You didn't say hello :()");
+}
+}
+var userInput;
+var response:
+
+userIput = prompt();
+response = processInput(userIput);
+
+alert(response);
